eec19cefa9b728472ce307026b04c78402d97fde
arikacips <1294330981oy@gmail.com>
2025-06-19 17:21:45 +0800

    Feat: Update default Gemini model

--- script.js
+++ script.js
@@ -37,15 +37,32 @@ document.addEventListener('DOMContentLoaded', () => {
         gemini: {
             baseUrl: 'https://generativelanguage.googleapis.com',
             models: [
-                'models/gemini-1.5-pro-latest', 'models/gemini-1.5-flash-latest', 'models/gemini-1.0-pro',
-                'models/gemini-2.0-flash', 'models/gemini-2.0-flash-001', 'models/gemini-2.0-flash-exp',
-                'models/gemini-2.0-flash-lite', 'models/gemini-2.0-flash-lite-001', 'models/gemini-2.0-flash-lite-preview',
-                'models/gemini-2.0-flash-lite-preview-02-05', 'models/gemini-2.0-flash-live-001', 'models/gemini-2.0-flash-thinking-exp',
-                'models/gemini-2.0-flash-thinking-exp-01-21', 'models/gemini-2.0-flash-thinking-exp-1219', 'models/gemini-2.0-pro-exp',
-                'models/gemini-2.0-pro-exp-02-05', 'models/gemini-2.5-flash', 'models/gemini-2.5-flash-lite-preview-06-17',
-                'models/gemini-2.5-flash-preview-04-17', 'models/gemini-2.5-flash-preview-04-17-thinking', 'models/gemini-2.5-flash-preview-05-20',
-                'models/gemini-2.5-pro', 'models/gemini-2.5-pro-exp-03-25', 'models/gemini-2.5-pro-preview-03-25',
-                'models/gemini-2.5-pro-preview-05-06', 'models/gemini-2.5-pro-preview-06-05'
+                'models/gemini-2.5-flash-preview-05-20',
+                'models/gemini-1.5-pro-latest',
+                'models/gemini-1.5-flash-latest',
+                'models/gemini-1.0-pro',
+                'models/gemini-2.0-flash',
+                'models/gemini-2.0-flash-001',
+                'models/gemini-2.0-flash-exp',
+                'models/gemini-2.0-flash-lite',
+                'models/gemini-2.0-flash-lite-001',
+                'models/gemini-2.0-flash-lite-preview',
+                'models/gemini-2.0-flash-lite-preview-02-05',
+                'models/gemini-2.0-flash-live-001',
+                'models/gemini-2.0-flash-thinking-exp',
+                'models/gemini-2.0-flash-thinking-exp-01-21',
+                'models/gemini-2.0-flash-thinking-exp-1219',
+                'models/gemini-2.0-pro-exp',
+                'models/gemini-2.0-pro-exp-02-05',
+                'models/gemini-2.5-flash',
+                'models/gemini-2.5-flash-lite-preview-06-17',
+                'models/gemini-2.5-flash-preview-04-17',
+                'models/gemini-2.5-flash-preview-04-17-thinking',
+                'models/gemini-2.5-pro',
+                'models/gemini-2.5-pro-exp-03-25',
+                'models/gemini-2.5-pro-preview-03-25',
+                'models/gemini-2.5-pro-preview-05-06',
+                'models/gemini-2.5-pro-preview-06-05'
             ]
         }
     };
